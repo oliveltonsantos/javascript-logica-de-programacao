3d6e48a6338d804a9df2525bdedb734bb0b4bc25
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-06-22 20:11:05 -0300

    update

--- exercicio-080/codigo.js
+++ exercicio-080/codigo.js
@@ -1,28 +1,73 @@
 const inputNumero = document.getElementById('numero')
+const btnVerificar = document.querySelector('button.btnVerificar')
+const btnRecomecar = document.querySelector('button.btnRecomecar')
 const resposta = document.querySelector('div.resposta')
 
 let listaNumeros = []
-let respostaFormatada
 
 function verificar() {
 
     const numeroDigitado = inputNumero.value
-    let numero = Number(numeroDigitado)
+    let numeroConvertido = Number(numeroDigitado)
+
+    if (numeroDigitado === '' || isNaN(numeroConvertido)) {
+        alert('Insira um número válido.')
+        inputNumero.value = ''
+        inputNumero.focus()
+        return
+    }
+
+    let exibicao = ''
 
     for (contador = 0; contador < 30; contador++) {
         let sorteio = Math.floor(Math.random() * 15) + 1
-
         listaNumeros.push(sorteio)
-
-        if (listaNumeros[contador] === numero) {
-            respostaFormatada = `${contador} - ${listaNumeros[contador]}`
-        }
-
     }
 
-    resposta.innerHTML += `<p>Números sorteados: ${listaNumeros.join(' | ')}</p>
-                        <p>Número encontrada nas posições: ${respostaFormatada}</p>
+    exibicao += `<p>Números sorteados: ${listaNumeros.join(' | ')}</p>`
 
-`
+    const numeroChave = listaNumeros
+        .map((valor, indice) => ({ indice, valor }))
+        .filter(item => item.valor === numeroConvertido)
+        .map(item => `[${item.indice}]`)
+        .join(' 👉 ')
+
+    if (numeroChave.length === 0) {
+        exibicao += '<p>O número não foi encontrado em nenhuma posição.</p>'
+    } else {
+        exibicao += `<p>Posições: ${numeroChave}</p>`
+    }
+
+    let numeroChaveQuantidade = 0
+
+    for (contador = 0; contador < listaNumeros.length; contador++) {
+        if (listaNumeros[contador] === numeroConvertido) {
+            numeroChaveQuantidade += 1
+        }
+    }
+
+    exibicao += `<p>Número ${numeroConvertido} encontrado ${numeroChaveQuantidade} vez(es).</p>`
+
+    resposta.innerHTML = `<p>${exibicao}</p>`
+
+    inputNumero.disabled = true
+
+    btnVerificar.style.display = 'none'
+    btnRecomecar.style.display = 'inline-block'
+
+}
+
+
+function recomecar() {
+    listaNumeros.length = 0
+
+    inputNumero.disabled = false
+    inputNumero.value = ''
+    inputNumero.focus()
+
+    btnVerificar.style.display = 'inline-block'
+    btnRecomecar.style.display = 'none'
+
+    resposta.innerHTML = ''
 
 }
