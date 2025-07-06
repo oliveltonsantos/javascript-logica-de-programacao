b1b6866c3d517e05305d770b0c94e2510fd465ba
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-07-06 20:15:25 -0300

    update

---
+++ exercicio-094/codigo.js
@@ -0,0 +1,56 @@
+const inputTermos = document.getElementById('termos')
+const btnGerarSequencia = document.querySelector('button.btnGerarSequencia')
+const btnNovaSequencia = document.querySelector('button.btnNovaSequencia')
+const resposta = document.querySelector('div.resposta')
+
+function gerarSequencia() {
+    const termosDigitado = inputTermos.value
+    const termoRecebido = Number(termosDigitado)
+
+    if (termosDigitado === '' || isNaN(termoRecebido) || termoRecebido <= 0) {
+        alert('Digite um valor maior que zero.')
+        inputTermos.value = ''
+        inputTermos.focus()
+        return
+    }
+
+    inputTermos.disabled = true
+    btnGerarSequencia.style.display = 'none'
+    btnNovaSequencia.style.display = 'inline-block'
+
+    fibonacci(termoRecebido)
+}
+
+function fibonacci(termoPassado) {
+
+    let sequenciaFibonacci = []
+
+    if (termoPassado >= 1) sequenciaFibonacci.push(1)
+    if (termoPassado >= 2) sequenciaFibonacci.push(1)
+
+    let termo1 = 1
+    let termo2 = 1
+
+    for (contador = 3; contador <= termoPassado; contador++) {
+
+        let termo3 = termo1 + termo2
+        termo1 = termo2
+        termo2 = termo3
+
+        sequenciaFibonacci.push(termo3)
+    }
+
+    resposta.innerHTML += `<p>Lista: ${sequenciaFibonacci.join(' >> ')} >> FIM</p>`
+
+}
+
+function novaSequencia() {
+
+    inputTermos.disabled = false
+    inputTermos.value = ''
+    inputTermos.focus()
+
+    btnGerarSequencia.style.display = 'inline-block'
+    btnNovaSequencia.style.display = 'none'
+    resposta.innerHTML = ''
+}
