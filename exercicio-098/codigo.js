66ff7bf455e2234a2eec8b68e44d7c6cce1f783c
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-07-09 11:59:06 -0300

    update

---
+++ exercicio-098/codigo.js
@@ -0,0 +1,73 @@
+const inputNumero1 = document.getElementById('numero1')
+const inputNumero2 = document.getElementById('numero2')
+const btnResultado = document.querySelector('button.btnResultado')
+const btnNovaSoma = document.querySelector('button.btnNovaSoma')
+const resposta = document.querySelector('div.resposta')
+
+function superSomador(numero1Passado, numero2Passado) {
+
+    const intervalo = numero2Passado - numero1Passado
+
+    const listaNumeros = []
+
+    let somaTotal = 0
+
+    for (let contador = 0; contador <= intervalo; contador++) {
+        listaNumeros.push(numero1Passado)
+        numero1Passado = numero1Passado + 1
+        somaTotal = somaTotal + listaNumeros[contador]
+    }
+
+    return `
+        <p>Sequência: ${listaNumeros.join(' > ')} > FIM!</p>
+        <p>Soma dos valores: ${somaTotal}</p>
+    `
+}
+
+function resultado() {
+    const numero1Digitado = inputNumero1.value
+    const numero1Convertido = Number(numero1Digitado)
+
+    if (numero1Digitado === '' || isNaN(numero1Convertido)) {
+        alert('Digite um número válido.')
+        inputNumero1.value = ''
+        inputNumero1.focus()
+        return
+    }
+
+    const numero2Digitado = inputNumero2.value
+    const numero2Convertido = Number(numero2Digitado)
+
+    if (numero2Digitado === '' || isNaN(numero2Convertido)) {
+        alert('Digite um número válido.')
+        inputNumero2.value = ''
+        inputNumero2.focus()
+        return
+    }
+
+    const resultadoRecebido = superSomador(numero1Convertido, numero2Convertido)
+
+    resposta.innerHTML = resultadoRecebido
+
+    inputNumero1.disabled = true
+    inputNumero2.disabled = true
+
+    btnResultado.style.display = 'none'
+    btnNovaSoma.style.display = 'inline-block'
+
+}
+
+function novaSoma() {
+    inputNumero1.disabled = false
+    inputNumero2.disabled = false
+    inputNumero1.value = ''
+    inputNumero2.value = ''
+    inputNumero1.focus()
+
+    btnResultado.style.display = 'inline-block'
+    btnNovaSoma.style.display = 'none'
+
+    resposta.innerHTML = ''
+}
+
+
